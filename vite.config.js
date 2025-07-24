469d86c0213e0def4d44d7315f12becad1c5225b
jlllyfish <james.chaigneaud@gmail.com>
2025-07-24 15:20:54 +0200

    pushy pushy

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    //
     outDir: "dist",
   },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["formulaire-erasmus-efp.up.railway.app"],
+  },
   server: {
-    port: 5173,
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
     proxy: {
       "/api/grist": {
         target: "https://grist.numerique.gouv.fr",
